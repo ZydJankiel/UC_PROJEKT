cb174403e4133d6f04cf59e44626f3ee8d9f9c57
Nosfreat <mdosiecki@gmail.com>
2021-07-06 15:08:50 +0200

    Update run.tcl

--- run.tcl
+++ run.tcl
@@ -60,6 +60,13 @@ read_verilog {
     src/rtl/font_rom.v
     src/rtl/char_rom_16x16.v
     src/rtl/draw_rect_char.v
+    src/rtl/list_ch04_11_mod_m_counter.v
+    src/rtl/list_ch04_15_disp_hex_mux.v
+    src/rtl/list_ch04_20_fifo.v
+    src/rtl/list_ch08_01_uart_rx.v
+    src/rtl/list_ch08_03_uart_tx.v
+    src/rtl/list_ch08_04_uart.v
+    src/rtl/top.v
 }
 
 read_vhdl {
